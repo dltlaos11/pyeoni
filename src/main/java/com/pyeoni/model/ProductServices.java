dda40686fcdf9aff51061082e5724822efab5230
Jennorresothie <head7579@gmail.com>
2023-04-20 12:02:34 +0900

    필요한 상품 검색 조건 구현

--- src/main/java/com/pyeoni/model/ProductServices.java
+++ src/main/java/com/pyeoni/model/ProductServices.java
@@ -15,8 +15,23 @@ public class ProductServices {
 		return pDAO.insertProduct(product);
 	}
 
-	// 상품 상세 조회
+	// 상품 개별 조회
 	public ProductVO detailProduct(String product_name, String promotion, String brand, int price) {
 		return pDAO.detailProduct(product_name, promotion, brand, price);	
+	}
+	
+	// 상품 편의점 회사별 조회
+	public List<ProductVO> selectProductByBrand(String brand){
+		return pDAO.selectProductByBrand(brand);
+	}
+	
+	// 상품 행사별 조회
+	public List<ProductVO> selectProductByPromotion(String promotion){
+		return pDAO.selectProductByPromotion(promotion);
+	}
+	
+	// 상품 유형별 조회
+	public List<ProductVO> selectProductByKind(String kind){
+		return pDAO.selectProductByKind(kind);
 	}
 }
