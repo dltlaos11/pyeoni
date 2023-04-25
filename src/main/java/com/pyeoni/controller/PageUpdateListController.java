a82d91bdb37b84e20d7cb13521b68bddf8465dbe
Jennorresothie <head7579@gmail.com>
2023-04-25 11:51:48 +0900

    더 보기 기능 구현4

--- src/main/java/com/pyeoni/controller/PageUpdateListController.java
+++ src/main/java/com/pyeoni/controller/PageUpdateListController.java
@@ -20,30 +20,27 @@ public class PageUpdateListController implements CommonControllerInterface {
 		System.out.println(method + " : 메소드");
 		HttpServletRequest req = (HttpServletRequest) data.get("request");
 
-		if (method.equals("GET")) {
-			
-			String brandValue = req.getParameter("brand");
+		String brandValue = req.getParameter("brand");
 
-			String searchValue = req.getParameter("search_bar");
+		String searchValue = req.getParameter("search_bar");
 
-			String sortValue = req.getParameter("sort_type");
+		String sortValue = req.getParameter("sort_type");
 
-			String eventValue = req.getParameter("event_type");
-			
-			String kindValue = req.getParameter("product_type");
-			
-			ProductServices services = new ProductServices();
-			List<ProductVO> productList = new ArrayList<>();
-			
-			start = req.getParameter("start") !=null? Integer.parseInt(req.getParameter("start")) : 1;
-			end = req.getParameter("end") !=null? Integer.parseInt(req.getParameter("end")) : 20;
-			
-			productList = services.selectAdvancedProduct(start, end, searchValue, sortValue, kindValue, eventValue, brandValue);
-			
-			req.setAttribute("productList", productList);
+		String eventValue = req.getParameter("event_type");
 
-		}
-		
+		String kindValue = req.getParameter("product_type");
+
+		ProductServices services = new ProductServices();
+		List<ProductVO> productList = new ArrayList<>();
+
+		start = req.getParameter("start") != null ? Integer.parseInt(req.getParameter("start")) : 1;
+		end = req.getParameter("end") != null ? Integer.parseInt(req.getParameter("end")) : 20;
+
+		productList = services.selectAdvancedProduct(start, end, searchValue, sortValue, kindValue, eventValue,
+				brandValue);
+
+		req.setAttribute("productList", productList);
+
 		return "../product/AddList.jsp";
 	}
 
